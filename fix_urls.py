ff33226d22a0675e10df3a45150cff5912cb09fd
Myles Braithwaite <me@mylesbraithwaite.com>
2016-03-05 16:20:45 -0500

    Fixed up some stuff.

--- fix_urls.py
+++ fix_urls.py
@@ -10,16 +10,19 @@ for root, dirname, filenames in os.walk('mylesbraithwaite.com'):
 for html_file in html_files:
     print(html_file)
 
+    with open(html_file, 'r') as f:
+        contents = f.read()
+
     with open(html_file, 'w+') as f:
         contents = f.read()
 
-        contents.replace('//media.mylesbraithwaite.com/', '/media/')
-        contents.replace('//static.mylesbraithwaite.com/', '/static/')
+        contentes = contents.replace('//media.mylesbraithwaite.com/', '/media/')
+        contentes = contents.replace('//static.mylesbraithwaite.com/', '/static/')
 
-        contents.replace('http:/media/', '/media/')
-        contents.replace('http:/static/', '/static/')
+        contentes = contents.replace('http:/media/', '/media/')
+        contentes = contents.replace('http:/static/', '/static/')
 
-        contents.replace('https:/media/', '/media/')
-        contents.replace('https:/static/', '/static/')
+        contentes = contents.replace('https:/media/', '/media/')
+        contentes = contents.replace('https:/static/', '/static/')
 
         f.write(contents)
